32ba65fcdbb5726c7229862e6ffe26850a954b7f
qpritchett <>
2024-09-27 17:57:36 -0600

    Add ViewModel

---
+++ app/src/main/java/com/example/quckdraw/DrawingViewModel.kt
@@ -0,0 +1,67 @@
+package com.example.quckdraw
+
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import android.graphics.Path
+import android.graphics.Paint
+
+class DrawingViewModel : ViewModel(){
+
+    private val drawingWidth = 500
+    private val drawingHeight = 500
+
+    // Bitmap to hold pixels, Canvas to host the draw calls, Paths for creating lines
+    // and a Paint for styles
+    private val _bitmap = MutableLiveData(Bitmap.createBitmap(drawingWidth, drawingHeight, Bitmap.Config.ARGB_8888))
+    private val canvas = Canvas(_bitmap.value!!)
+    private val paint: Paint = Paint().apply {
+        color = Color.BLUE
+        style = Paint.Style.STROKE
+        strokeWidth = 10f
+        isAntiAlias = true
+    }
+    private var currentPath: Path = Path()
+
+    // Store all the paths of the drawing as a list 
+    private val paths: MutableList<Path> = mutableListOf()
+
+    private val _penColor : MutableLiveData<Color> = MutableLiveData(Color.valueOf(0f, 0f, 1f))
+    val penColor = _penColor as LiveData<Color>
+    private val _penSize = MutableLiveData(10f)
+    val penSize = _penSize as LiveData<Float>
+
+
+    // Start a new path and move to the touch position
+    fun startPath(x: Float, y: Float) {
+        currentPath = Path()
+        currentPath.moveTo(x, y)
+        paths.add(currentPath)
+    }
+
+    // Add new lines to the path and add draw them on the canvas
+    fun addToPath(x: Float, y: Float) {
+        currentPath.lineTo(x, y)
+        canvas.drawPath(currentPath, paint)
+    }
+
+    // Draw all paths on the canvas
+    private fun drawAllPaths() {
+        paths.forEach { path ->
+            canvas.drawPath(path, paint)
+        }
+    }
+
+    fun setPenColor(color: Color) {
+        _penColor.value = color
+        paint.color = color.toArgb()
+    }
+
+    fun setPenSize(size: Float) {
+        _penSize.value = size
+        paint.strokeWidth = size
+    }
+}
