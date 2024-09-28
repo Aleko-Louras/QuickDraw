eff59344efc9e883e9a8ca568349d155990e3859
Joeic <cqy1508460399@gmail.com>
2024-09-27 18:00:19 -0600

    clean code

--- app/src/main/java/com/example/quckdraw/DrawingView.kt
+++ app/src/main/java/com/example/quckdraw/DrawingView.kt
@@ -10,11 +10,8 @@ import android.view.MotionEvent
 import android.view.View
 
 class DrawingView(context: Context, attrs: AttributeSet?) : View(context, attrs) {
-
-    // 用于存储绘制路径的 Path 对象
     private var path = Path()
 
-    // 创建一个 Paint 对象用于绘制蓝色线条
     private val paint = Paint().apply {
         color = Color.BLUE
         isAntiAlias = true
@@ -26,7 +23,6 @@ class DrawingView(context: Context, attrs: AttributeSet?) : View(context, attrs)
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        // 绘制当前的路径
         canvas.drawPath(path, paint)
     }
 
@@ -37,16 +33,13 @@ class DrawingView(context: Context, attrs: AttributeSet?) : View(context, attrs)
 
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-                // 当用户按下时，移动路径的起点到当前触摸点
                 path.moveTo(x, y)
             }
             MotionEvent.ACTION_MOVE -> {
-                // 当用户移动时，绘制线条到当前触摸点
                 path.lineTo(x, y)
-                invalidate() // 刷新视图，触发 onDraw() 重新绘制
+                invalidate()
             }
             MotionEvent.ACTION_UP -> {
-                // 当用户抬起时，不需要特别处理
             }
         }
 
