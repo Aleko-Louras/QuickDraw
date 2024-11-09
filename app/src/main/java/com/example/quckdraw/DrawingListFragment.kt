77e1e458ef1858bdaa155ece941a90a0fdc31a78
Joeic <cqy1508460399@gmail.com>
2024-11-08 18:36:52 -0700

    Update DrawingListFragment.kt

--- app/src/main/java/com/example/quckdraw/DrawingListFragment.kt
+++ app/src/main/java/com/example/quckdraw/DrawingListFragment.kt
@@ -1,18 +1,13 @@
 package com.example.quckdraw
 
-import android.view.Surface
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import org.w3c.dom.Text
-import java.lang.reflect.Modifier
 
 
 @Composable
